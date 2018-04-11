1c50c527b52c4bc8599e630548cde74540b85e7a
harim1206 <harim1206@gmail.com>
2018-04-11 17:52:33 -0400

    finished

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,46 @@
 class DogsController < ApplicationController
+    before_action :get_dog, only: [:show, :edit, :update, :delete]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+
+  end
+
+  # def new
+  #   @dog = Dog.new
+  #   @employees = Employee.all
+  #
+  #
+  # end
+  #
+  # def create
+  #
+  #
+  # end
+  #
+  # def edit
+  #
+  # end
+  #
+  # def update
+  #
+  # end
+  #
+  # def delete
+  #
+  # end
+
+
+  
+  private
+  def get_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age, employees: [])
+  end
 end
